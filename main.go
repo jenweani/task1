72360a1481f2ff9c587fe8a46d1b119b2da627e2
john <jenweani@gmail.com>
2023-09-07 15:52:46 +0100

    init commit

--- main.go
+++ main.go
@@ -7,18 +7,17 @@ import (
 	"time"
 )
 
-type Task struct{
-	Slack_name string `json:"slack_name"`
-	Current_day string `json:"current_day"`
-	Utc_time time.Time `json:"utc_time"`
-	Track string `json:"track"`
-	Github_file_url string `json:"github_file_url"`
-	Github_repo_url string `json:"github_repo_url"`
-	Status_code int `json:"status_code"`
+type Task struct {
+	Slack_name      string    `json:"slack_name"`
+	Current_day     string    `json:"current_day"`
+	Utc_time        time.Time `json:"utc_time"`
+	Track           string    `json:"track"`
+	Github_file_url string    `json:"github_file_url"`
+	Github_repo_url string    `json:"github_repo_url"`
+	Status_code     int       `json:"status_code"`
 }
 
-
-func main(){
+func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "John's hngx task 1")
 	})
@@ -28,13 +27,13 @@ func main(){
 		track := r.URL.Query().Get("track")
 
 		task1 := Task{
-			Slack_name: slack_name,
-			Current_day: time.Now().Weekday().String(),
-			Utc_time: time.Now(),
-			Track: track,
+			Slack_name:      slack_name,
+			Current_day:     time.Now().Weekday().String(),
+			Utc_time:        time.Now(),
+			Track:           track,
 			Github_file_url: "github.com/jenweani/",
 			Github_repo_url: "github.com/jenweani/",
-			Status_code: http.StatusOK,
+			Status_code:     http.StatusOK,
 		}
 
 		json.NewEncoder(w).Encode(task1)
